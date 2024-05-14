cc954aec65e9abf33819b8b7ae573cc4817cae6b
Sourabh Munde <sourabh.munde15@gmail.com>
2024-05-14 20:43:35 +0530

    update: packages

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,8 +11,7 @@ async function main() {
   console.log("Crowdfunding deployed to:", crowdfunding.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main()
   .then(() => process.exit(0))
   .catch((error) => {
